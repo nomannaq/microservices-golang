6239fece7c8247cbb223ccc1152b65c9c1f1d863
Noman <nomannaq.aa@gmail.com>
2025-06-16 15:14:52 +0300

    Fix, Duplicate Path in render function

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -12,8 +12,8 @@ func main() {
 		render(w, "test.page.gohtml")
 	})
 
-	fmt.Println("Starting front end service on port 80")
-	err := http.ListenAndServe(":80", nil)
+	fmt.Println("Starting front end service on port 3000")
+	err := http.ListenAndServe("localhost:3000", nil)
 	if err != nil {
 		log.Panic(err)
 	}
@@ -22,13 +22,13 @@ func main() {
 func render(w http.ResponseWriter, t string) {
 
 	partials := []string{
-		"./cmd/web/templates/base.layout.gohtml",
-		"./cmd/web/templates/header.partial.gohtml",
-		"./cmd/web/templates/footer.partial.gohtml",
+		"templates/base.layout.gohtml",
+		"templates/header.partial.gohtml",
+		"templates/footer.partial.gohtml",
 	}
 
 	var templateSlice []string
-	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", t))
+	templateSlice = append(templateSlice, fmt.Sprintf("templates/%s", t))
 
 	for _, x := range partials {
 		templateSlice = append(templateSlice, x)

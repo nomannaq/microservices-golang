f044a4cb295d6b20ffb09ed214f45892ffa1b654
Noman <nomannaq.aa@gmail.com>
2025-06-19 18:48:57 +0300

    Broker service is working now

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -12,9 +12,8 @@ type Config struct{}
 
 func main() {
 	fmt.Println("Starting broker service on port 4000")
-	app := Config{}
 
-	log.Println("Starting broker service on port %s", webPort)
+	log.Printf("Starting broker service on port %s", webPort)
 	srv := &http.Server{
 		Addr: fmt.Sprintf(":%s", webPort),
 	}
--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -19,6 +19,6 @@ func (app *Config) routes() http.Handler {
 		MaxAge:           300,
 	}))
 	mux.Use(middleware.Heartbeat("/ping"))
-
+	mux.Post("/", app.Broker)
 	return mux
 }
